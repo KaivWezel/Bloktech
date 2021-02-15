6190fa3e2b812f489f9454943b3e7fd4525ee613
KaivWezel <kaivanwezel@gmail.com>
2021-02-15 22:35:49 +0100

    HTML form version 0.1

--- static/app.js
+++ static/app.js
@@ -5,11 +5,14 @@ hobbies.forEach(function (hobby) {
   const newListItem = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("id", hobby);
+
   label.setAttribute("for", hobby);
+  label.innerText = hobby;
+
   list.appendChild(newListItem);
   newListItem.appendChild(checkbox);
   newListItem.appendChild(label);
-  label.innerText = hobby;
 });
