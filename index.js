691a01ce69efe143d0f0e7f47aced0a8edbbce75
KaivWezel <kaivanwezel@gmail.com>
2021-03-04 11:11:37 +0100

    profiles available

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-require("dotenv/config");
-const express = require("express");
-const app = express();
-const camelCase = require("camelcase");
-const ejs = require("ejs");
-const port = 4000;
-const multer = require("multer");
-const path = require("path");
-const urlencodedParser = express.urlencoded();
-const mongoose = require("mongoose");
-const user = require("./static/models/user");
-const fs = require("fs");
-const { ObjectId } = require("mongodb");
-const { callbackify } = require("util");
-const url = process.env.DB_HOST;
+require('dotenv/config')
+const express = require('express')
+const app = express()
+const camelCase = require('camelcase')
+const ejs = require('ejs')
+const port = 4000
+const multer = require('multer')
+const path = require('path')
+const urlencodedParser = express.urlencoded()
+const mongoose = require('mongoose')
+const user = require('./static/models/user')
+const fs = require('fs')
+const { ObjectId } = require('mongodb')
+const { callbackify } = require('util')
+const { profile } = require('console')
+const url = process.env.DB_HOST
 
 //connect to mongodb with mongoose
 mongoose
@@ -21,54 +22,54 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("connected to Mongo");
+    console.log('connected to Mongo')
   })
   .catch((err) => {
-    console.log("error", err);
-  });
+    console.log('error', err)
+  })
 
 //set storageengine Multer
 const storage = multer.diskStorage({
-  destination: "./static/uploads/",
+  destination: './static/uploads/',
   filename: function (req, file, cb) {
     cb(
       null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    )
   },
-});
+})
 
 //init upload
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
 //serve static files
-app.use(express.static("static"));
+app.use(express.static('static'))
 
 //set view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
 app.listen(port, () => {
-  console.log(`listening at port ${port}`);
-});
+  console.log(`listening at port ${port}`)
+})
 
 //get create profile
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-app.get("/profile/create", (req, res) => {
-  res.render("create");
-});
+app.get('/profile/create', (req, res) => {
+  res.render('create')
+})
 
 // current user Id storage
-let currentUserId;
+let profileId
 
 //receive created profile
 app.post(
-  "/profile/create",
+  '/profile/create',
   urlencodedParser,
-  upload.single("upload"),
-  (req, res, next) => {
+  upload.single('upload'),
+  async (req, res, next) => {
     const newUser = new user({
       name: req.body.name,
       birthdate: req.body.birthdate,
@@ -76,40 +77,71 @@ app.post(
       hobbies: req.body.hobbies,
       bio: req.body.bio,
       img: req.file.filename,
-    });
-    newUser.save((err, user) => {
-      if (err) {
-        console.log(err);
-      } else {
-        currentUserId = user.id;
-        console.log(currentUserId);
-      }
-    });
-    next();
+    })
+    newUser
+      .save()
+      .then((user) => {
+        profileId = user.id
+      })
+      .then(() => {
+        next()
+      })
   },
   (req, res) => {
-    res.render("profile", {
-      profile: req.body,
-    });
+    //Waarom werkt dit niet met een promise?
+    // user.findById(currentUserId).then((err, result) => {
+    //   console.log("gevonden");
+    //   res.render("profile", {
+    //     profile: result,
+    //   });
+    // });
+    user.findById(profileId, done)
+    function done(err, result) {
+      console.log('gevonden')
+      res.render('profile', {
+        profile: result,
+      })
+    }
   }
-);
+)
 
 //edit profile
-app.post("/profile/edit", (req, res) => {
-  let currentUser;
-  user.findById(currentUserId).then((result) => {
-    console.log("gevonden");
-    currentUser = result;
-    console.log(currentUser);
-  });
-  res.send(JSON.stringify(currentUser));
-});
+app.post('/profile/edit', (req, res) => {
+  user.findById(profileId, done)
+  function done(err, result) {
+    console.log('gevonden')
+    console.log(result)
+    res.render('edit', {
+      profile: result,
+    })
+  }
+})
 
+//profiles list
+app.get('/profiles', (req, res, profiles) => {
+  user.find().then((result) => {
+    console.log(profiles)
+    res.render('profiles', {
+      profiles: result,
+    })
+  })
+})
+
+//request for selected profile
+app.get('/profile/:profileId', (req, res) => {
+  profileId = req.params.profileId
+  user.findById(profileId, done)
+  function done(err, result) {
+    res.render('profile', {
+      profile: result,
+    })
+  }
+})
 //404
 app.use((req, res) => {
-  res.status(404).send("this page does not exist.");
-});
+  res.status(404).send('this page does not exist.')
+})
 
 // user.remove({}, () => {
-//   console.log("verwijderd");
-// });
+//   console.log('verwijderd')
+// })
